cc4f5ee69f262c7768bd2be24497b1326950c943
Kien Tran <kientt15@onemount.com>
2021-11-10 02:55:26 +0700

    clean up crawler and craw 3000 records

--- puzzles/websudoku_spider.py
+++ puzzles/websudoku_spider.py
@@ -1,24 +1,27 @@
 import scrapy
 import time
 import numpy as np
+import itertools
 
 class WebSudoku(scrapy.Spider):
     name = 'websudoku'
-    download_delay = 5.1932
+    # download_delay = 5.1932
 
     def start_requests(self):
-        for level in range(1,5):
-            for id in range(1000):
-                yield scrapy.Request(
-                    url=f'http://nine.websudoku.com/?select=0&level={level}&set_id={id}',
-                    callback=self.parse
-                )
+        size = 10000
+        level_list = [np.random.randint(1,5) for i in range(size)]
+        id_list = [np.random.randint(10000,20000) for i in range(size)]
+        for level, id in zip(level_list, id_list):
+            yield scrapy.Request(
+                url=f'http://nine.websudoku.com/?select=0&level={level}&set_id={id}',
+                callback=self.parse
+            )
                 
-                time.sleep(np.random.rand() + 0.5)
 
     def parse(self, response):
         yield {
             'level': response.xpath('//*[@name="level"]/@value').get(),
+            'id': response.xpath('//*[@id="pid"]/@value').get(),
             'result': response.xpath('//*[@id="cheat"]/@value').get(),
             'display': response.xpath('//*[@id="editmask"]/@value').get()\
             .replace('0', '.').replace('1', '0').replace('.','1')
